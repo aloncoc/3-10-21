63f09536359808cf74d957b9064550246fea9f77
aloncoc <89935432+aloncoc@users.noreply.github.com>
2021-10-05 13:51:18 +0300

    Create homework (3).py

---
+++ homework (3).py
@@ -0,0 +1,11 @@
+i=1
+while i<=5:
+    temp=int(input('enter number:'))
+    if temp >40 or temp <5:
+        print('wrong temp')
+        break
+    else:
+        print('vaild temp')
+        i+=1
+
+
